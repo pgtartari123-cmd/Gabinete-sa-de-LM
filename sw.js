96e7e8ccf405f1f9dfc8b80f47cd9121980d4256
pgtartari123-cmd <pgtartari123@gmail.com>
2026-09-15 19:37:49 -0300

    Atualiza cache do Service Worker para o banco central

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='gabinete-lm-shell-v29';
+const CACHE_NAME='gabinete-lm-shell-v30';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest?v=3008','./style.css?v=2002'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
